dfd3083c2a8e4d2118feb9268be023ea401062dc
JT <jtpopoola07@gmail.com>
2024-08-22 14:55:00 -0700

    added js scripts

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// let a = 2
+// let b = 3
+// let c = a + b
+// console.log(c)
+// console.log('hello world')
+// console.log('the addition of ' + a + " and " + b + ' is equals to ' + c )
+// let a = parseInt( prompt('enter your value for a'))
+// let b = parseInt(prompt('enter your value for b'))
+// let c = parseInt(prompt('enter your value for c'))
+// let d = a * b * c
+// console.log(d)
+// console.log(`The multiplication of ${a} and ${b} and ${c} is equals to ${d}`)
+// console.log(2 + parseInt("2"))
+
+
+// If statements
+// let a = parseInt(prompt("Enter a number"))
+// let a = "0"
+// if(a > 0){
+//     alert("It's a positive number")
+// } else if(a < 0){
+//     alert("It's a negative number")
+// }else if(a === 0){
+//     alert("it's neutral")
+// }else{
+//     alert("What did you write")
+// }
+
+
+let c = parseInt(prompt('Enter a number'))
+
+let d = c % 2
+console.log(2)
+
+if(d == 1){
+    alert("It's an odd number")
+} else if(d == 0){
+    alert("It's an even")
+}
